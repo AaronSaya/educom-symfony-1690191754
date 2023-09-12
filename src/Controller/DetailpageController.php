c0a5bd2386512892e887b92d61a0e3dd47b730a7
Aaron Saya <60886005+AaronSaya@users.noreply.github.com>
2023-09-12 12:40:30 +0200

    Back-end #3

--- src/Controller/DetailpageController.php
+++ src/Controller/DetailpageController.php
@@ -7,14 +7,17 @@ use App\Service\VacanciesService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class DetailpageController extends AbstractController
 {
     private $vacanciesService;
+    private $security;
 
-    public function __construct(VacanciesService $vacanciesService)
+    public function __construct(VacanciesService $vacanciesService, Security $security)
     {
         $this->vacanciesService = $vacanciesService;
+        $this->security = $security;
     }
 
     #[Route('/detailpage', name: 'app_detailpage')]
@@ -23,10 +26,21 @@ class DetailpageController extends AbstractController
         $vacancy = $this->vacanciesService->getVacanciesById($id);
 
         if ($vacancy) {
-          $this->vacanciesService->removeVacancy($vacancy);
-      }
+            $this->vacanciesService->removeVacancy($vacancy);
+        }
         return $this->render('detailpage/index.html.twig', [
             'vacancy' => $vacancy,
         ]);
+    }
+
+    #[Route('/detailpage/apply/{id}', name: 'apply_detailpage')]
+    public function applyVacancy($id)
+    {
+        if (!$this->security->isGranted('IS_AUTHENTICATED_FULLY')) {
+           
+            return $this->redirectToRoute('app_login');
+        }
+      
+        return $this->redirectToRoute('app_detailpage', ['id' => $id]);
     }
 }
